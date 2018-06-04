0d768e734f7d96c7de0fb09b0bdb385232a3c0cf
方云鹤 <842241090@qq.com>
2018-06-04 16:33:10 +0800

    添加注释

--- src/main/java/org/fyh/zk/lock/ZkDestributeLockApplication.java
+++ src/main/java/org/fyh/zk/lock/ZkDestributeLockApplication.java
@@ -4,6 +4,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * @Description:zk分布式锁启动类
+ * @author:fangyunhe
+ * @time:2018年6月4日 下午4:31:49
+ * @version 1.0
+ */
 @MapperScan("org.fyh.zk.lock.dao")
 @SpringBootApplication
 public class ZkDestributeLockApplication {
